1ce80ce18b03dc8688b2fc9702934b23f05c15f9
Sukrut Datar <>
2019-11-07 17:58:12 +0530

    implemented advanced remediation solution

--- DLL Hijacking/VictimApplication/VictimApplication.cpp
+++ DLL Hijacking/VictimApplication/VictimApplication.cpp
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include <atlstr.h>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -12,11 +13,32 @@ using namespace std;
 
 #pragma comment(lib, "../Release/Arithmatic.lib")
 
+#define DLL_NAME "Arithmatic.dll"
 #define SHA256 "651618d94d75571f8abd2a480c6368e124a0542a2bc0e6a10be7ca6e455570e9"
 
-BOOL Authenticate()
+std::string GetModulePath()
 {
-	ifstream istream("Arithmatic.dll", std::ios::in | std::ios::binary);
+	HMODULE hArithmaticModule = LoadLibraryEx(TEXT(DLL_NAME), NULL, DONT_RESOLVE_DLL_REFERENCES);
+	if (hArithmaticModule)
+	{
+		TCHAR dllPath[_MAX_PATH];
+		HMODULE hModule = GetModuleHandle(TEXT(DLL_NAME));
+		if (hModule)
+		{
+			GetModuleFileName(hModule, dllPath, _MAX_PATH);
+			FreeLibrary(hArithmaticModule);
+			std::wstring path = std::wstring(dllPath);
+			return std::string(path.begin(), path.end());
+		}
+	}
+	return "";
+}
+
+
+BOOL Authenticate(std::string* safeLoadPath)
+{
+	safeLoadPath = new std::string(GetModulePath());
+	ifstream istream(safeLoadPath->c_str(), std::ios::in | std::ios::binary);
 	
 	std::string content = std::string((std::istreambuf_iterator<char>(istream)), (std::istreambuf_iterator<char>()));
 	istream.close();
@@ -41,25 +63,43 @@ BOOL Authenticate()
 }
 
 
-int main()
+int main(int argc, char** argv)
 {
-
-	if (!Authenticate())
+	bool safeLoad = false;
+	std::string* safeLoadPath = new std::string(DLL_NAME);
+	if (argc == 2)
 	{
-		cout << endl << "Malicious DLL found, load dll fail.";
-		return 0;
+		std::string argument(argv[1]);
+		if (argument == "-safe")
+			safeLoad = true;
 	}
+
+	if (safeLoad)
+	{
+		if (!Authenticate(safeLoadPath))
+		{
+			cout << endl << "Malicious DLL found, load dll fail.";
+			return 0;
+		}
+	}
+
 	int a = 10;
 	int b = 20;
-	std::cout << std::endl << "maximum(" << a << ", " << b << ") = " << maximum(a, b);
+	
 
-	HMODULE hArithmaticModule = LoadLibrary(TEXT("Arithmatic.dll"));
+	HMODULE hArithmaticModule = LoadLibrary(std::wstring(safeLoadPath->begin(), safeLoadPath->end()).c_str());
 	if (hArithmaticModule != NULL)
 	{
-		typedef int (*Add)(int, int); 
-		Add add = (Add)GetProcAddress(hArithmaticModule, "add");
+		typedef int (*Arithmatic)(int, int); 
+		Arithmatic add = (Arithmatic)GetProcAddress(hArithmaticModule, "add");
 		if (add)
 			std::cout << std::endl << "add(" << a << ", " << b << ") = " << add(a, b);
+
+		Arithmatic maximum = (Arithmatic)GetProcAddress(hArithmaticModule, "maximum");
+		if (maximum)
+		std::cout << std::endl << "maximum(" << a << ", " << b << ") = " << maximum(a, b);
+
+		FreeLibrary(hArithmaticModule);
 	}
 
 	std::cout << std::endl << std::endl;
